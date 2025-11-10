e8caf46271faaa16eb4fa497d9a72dc7b445baae
mathewo20 <mo237704937@gmail.com>
2025-11-10 15:12:39 +0000

    i finsihed my loop and i finished the nurmerical part of the code

--- mian.py
+++ mian.py
@@ -1,8 +1,13 @@
 print("What is the weather forcast for tomorrow")
+while True:
+    temp = input("Temperature: " )
+    try:
+        float(temp)
+        break
+    except ValueError:
+        print("please enter a valid number for temperature")
 
-temp = input("Temperature: " )
-
-print("the Temperature is " + (temp))
+print("the Temperature is " + temp)
 
 print("would you like it to be converted into fahrenheit ? ")
 
@@ -11,8 +16,11 @@ user_choice = input("yes or no ?: ")
 
 try:
     if user_choice == "yes":
-        fahrenheit = temp * 9/5 + 32
+        fahrenheit = float(temp) * 9/5 + 32
+        print(fahrenheit)
     else:
-        print("okay i wont convert it")
+         print("okay i wont convert it")
+
 except ValueError:
-    print("please enter a valid response")
+    print("please enter a valid number for temperature")
+
