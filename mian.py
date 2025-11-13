60e44374c98afa9c538b0bcfca8d1e52a89614aa
mathewo20 <mo237704937@gmail.com>
2025-11-13 14:20:49 +0000

    almost finsihed

--- mian.py
+++ mian.py
@@ -24,7 +24,25 @@ try:
 except ValueError:
     print("please enter a valid number for temperature") 
 
-print("is it going to rain tomorrow ? ")
-while True: 
+if float(temp) > 20:
+    print("wow its going to be a hot day")
+
+print("will it rain ?")
+
+rain = input("yes or no ?: ")
+
+if rain == "yes":
+    print("have an umbrella with you or a rain coat be fly and dry")
+else:
+    print("enjoy the weather and wear some baggy jeans and a t shirt or if your not a fan of baggy jeans then just wear some shorts and a t shirt")
+
+if float(temp) < 10:
+    print("woah thats a lil cold not really my liking ")
+print("will it rain ?")
+
+rain = input("yes or no ?: ")
+
+
+
     
 
